168057e5700bb23acbff428a18a256aa6f546ec1
ewalk40 <walkereric10@gmail.com>
2019-10-27 23:28:47 -0400

    Added launch code

--- CheckForFirstTimeLaunchDemo/ViewController.swift
+++ CheckForFirstTimeLaunchDemo/ViewController.swift
@@ -2,10 +2,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        if defaults.bool(forKey: "First Launch") == true {
+            
+            print("Second+")
+            
+            // Run Code After First Launch
+            
+            defaults.set(true, forKey: "First Launch")
+            
+        } else {
+            
+            
+            print("First")
+            
+            // Run Code During First Launch
+
+            defaults.set(true, forKey: "First Launch")
+            
+        }
         
     }
 
